c7f5ce0ed04580ea3c3b1ccd5329fb28f1dcf5b3
Vagya <shuklavagya@gmail.com>
2026-05-30 01:06:54 +0530

    add variables and datatypes practice

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("---DAY 1---")
+let name = "Vagya"
+let college = "JIIT"
+let age = 19
+let canVote=true
+let value=null
+console.log("Hello, my name is " + name )
+console.log("I study at " + college)
+console.log("2 + 3 =", 2 + 3)
+console.log("Node.js is working!")
+console.log(typeof name)
+console.log(typeof college)
+console.log(typeof age)
+console.log(typeof canVote)
+console.log(typeof value)
+console.log("5"/2)
+console.log("5"*2)
+console.log("5"+2)
+console.log("5"-2)
